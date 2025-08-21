fd730ed82b7e40902a18e792612bf903b856c336
Muzzu153 <muzzusyed153@gmail.com>
2025-08-21 13:31:04 +0530

    Make an array of personal info

---
+++ src/data/personal.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Syed Muzaffer Ali",
+  age: 22,
+  education: "MCA",
+  role: "Full Stack Web Developer",
+  tagline: "Building Digital Solutions with Code & Creativity",
+  about: "Hey! I'm Muzaffer, a 22-year-old full-stack developer currently pursuing my MCA. I love turning complex problems into elegant digital solutions. When I'm not debugging code at 2 AM, I'm exploring new tech stacks and building things that actually matter to people. I believe great software should be functional, beautiful, and accessible to everyone.",
+  availability: "Available for Freelance Work",
+  responseTime: "Usually responds within 24 hours",
+  location: "India",
+  email: "muzzusyed153@gmail.com",
+  phone: "+91 XXXXX XXXXX",
+  social: {
+    github: "https://github.com/muzzu153",
+    linkedin: "#",
+    twitter: "#"
+  }
+};
