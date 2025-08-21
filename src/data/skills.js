f51cc8f43316126b2eac0f575e039802324b23cf
Muzzu153 <muzzusyed153@gmail.com>
2025-08-21 13:32:29 +0530

    Make skills array for mapping through it

---
+++ src/data/skills.js
@@ -0,0 +1,70 @@
+export const skillsData = [
+  {
+    category: "Frontend",
+    skills: [
+      {
+        name: "HTML5",
+        icon: "icons/HTML_logo.svg",
+      },
+      {
+        name: "CSS3",
+        icon: "icons/CSS_logo.svg",
+      },
+      {
+        name: "JavaScript",
+        icon: "icons/JSLogo.svg",
+      },
+      {
+        name: "React.js",
+        icon: "icons/react_logo.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "icons/tailwindcss_logo.svg",
+        icon2: "icons/tailwindcss_logo_dark.svg",
+      },
+    ],
+  },
+  {
+    category: "Backend",
+    skills: [
+      {
+        name: "Node.js",
+        icon: "icons/nodejs_logo.svg",
+      },
+      {
+        name: "Express.js",
+        icon: "icons/express_logo.svg",
+      },
+    ],
+  },
+
+  {
+    category: "Databases & ORM/ODM",
+    skills: [
+      { name: "MongoDB", icon: "icons/mongoDB_logo.svg", icon2: "icons/mongoDB_logo_dark" },
+      { name: "PostgreSQL", icon: "icons/postgresql_logo.svg" },
+      { name: "Mongoose", icon: "icons/mongoose_logo.svg" },
+      { name: "Prisma ORM", icon: "icons/prisma-orm_logo.svg" },
+    ],
+  },
+
+  {
+    category: "Tools",
+    skills: [
+      {
+        name: "Vite",
+        icon: "icons/vitejs_logo.svg",
+      },
+      {
+        name: "Webpack",
+        icon: "icons/webpack_logo.svg",
+        icon1: "icons/webpack_logo_light.svg",
+      },
+      {
+        name: "Git",
+        icon: "icons/git_logo.svg",
+      },
+    ],
+  },
+];
