ca1f99dbde2210765eed483a72e6cdb03c58af4d
Muzzu153 <muzzusyed153@gmail.com>
2025-09-25 21:19:05 +0530

    Revert postcss.config.mjs to tailwind v4 configs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,5 @@
 export default {
   plugins: {
-
-    tailwindcss: {
-      // This is the crucial part you need to add!
-      content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-    },
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   }
 } 
