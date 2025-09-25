8425c40a00c5de8bb2d5ac47ef3604a0be70b560
Muzzu153 <muzzusyed153@gmail.com>
2025-09-25 20:19:33 +0530

    Make changes to postcss.config.mjs file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,13 @@
 export default {
   plugins: {
     "@tailwindcss/postcss": {},
+    tailwindcss: {
+      // This is the crucial part you need to add!
+      content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+    },
+    autoprefixer: {},
   }
 } 
