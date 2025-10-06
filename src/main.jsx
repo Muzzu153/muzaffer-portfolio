f943b58a29e64d8a253206a4823bb6743c274b0d
Muzzu153 <muzzusyed153@gmail.com>
2025-10-06 09:38:52 +0530

    formatting main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createRootRoute, createRoute, createRouter, RouterProvider, lazyRouteComponent } from '@tanstack/react-router'
-// import HomePage from './components/pages/HomePage.jsx'
-// import BlogListPage from './components/pages/BlogListPage.jsx'
-// import BlogPostPage from './components/pages/BlogPostPage.jsx'
-
 
 const rootRoute = createRootRoute({
   component: App
