c6f83d8a5370684cd964ff8f79339472e167b131
Muzzu153 <muzzusyed153@gmail.com>
2025-10-10 15:04:11 +0530

    Modify App.jsx for tanstack query and unhead/react Wrap the layout in <QueryClientProvider> and <UnheadProvider>

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import { lazy, Suspense } from "react";
 import { Outlet } from "@tanstack/react-router";
+import { QueryClientProvider } from '@tanstack/react-query';
+// import * as myHead from '@unhead/react/client'
+import { queryClient } from "./utils/queryKeys";
+import { Head } from "@unhead/react";
+import { createHead, UnheadProvider } from "@unhead/react/client";
 
 const Navbar = lazy(() => import("./components/ui/Navbar"))
 const Footer = lazy(() => import("./components/sections/Footer"))
 
+const head = createHead()
+
 const App = () => {
   return (<>
-    <Navbar />
 
-    <Suspense>
-      <Outlet />
-    </Suspense>
+    <UnheadProvider head={head}>
+      <QueryClientProvider client={queryClient}>
 
-    <Suspense>
-      <Footer />
-    </Suspense>
+        <Navbar />
+
+        <Suspense>
+          <Outlet />
+        </Suspense>
+
+        <Suspense>
+          <Footer />
+        </Suspense>
+
+      </QueryClientProvider>
+    </UnheadProvider>
+
+
   </>)
 }
 
